6c84a0c52cc0518e5cee7b5b41c7b6efdaa63ee6
3uc1id <3uc1id.dev@gmail.com>
2025-02-18 11:30:24 -0800

    refactor clock code a bit

--- clock.js
+++ clock.js
@@ -1,4 +1,5 @@
 const TAU = 6.283185307179586;
+const QTAU = TAU/4.0;
 const TICK = 100; // ms per frame
 const RADIUS = 100; // radius of clock circle
 const WIDTH = 250; // canvas width
@@ -13,37 +14,51 @@ const SECOND_LENGTH = RADIUS * 0.9;
 const MINUTE_LENGTH = RADIUS * 0.8;
 const HOUR_LENGTH = RADIUS * 0.5;
 
+// Stroke widths
+const CLOCK_BORDER_WIDTH = 2;
+const HOUR_MARK_WIDTH = 2;
+const MINUTE_MARK_WIDTH = 1;
+const SECOND_HAND_WIDTH = 1;
+const MINUTE_HAND_WIDTH = 2;
+const HOUR_HAND_WIDTH = 5;
+
 
 function drawClock() {
     canvas = document.getElementById("clock");
     ctx = canvas.getContext("2d");
+    ctx.translate(HW, HH); // move origin to center of canvas
+    ctx.scale(1.0, -1.0); // flip y axis so it goes up instead of down
     drawHelper(ctx);
 }
 
 function drawHelper(ctx) {
     // clear the canvas
-    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+    // Normally clearRect takes the top left corner of the rectangle to clear,
+    // along with the width and height. But since the coordinate transform flipped the
+    // y-axis, it needs the bottom left corner instead.
+    ctx.clearRect(-HW, -HH, WIDTH, HEIGHT);
 
-    // draw ciruclar clock outline
+    // draw circular clock outline
     ctx.strokeStyle = "black";
-    ctx.lineWidth = 2;
+    ctx.lineWidth = CLOCK_BORDER_WIDTH;
     ctx.beginPath();
-    ctx.arc(125,125,100,0,TAU);
+    ctx.arc(0.0, 0.0, RADIUS, 0.0, TAU);
     ctx.stroke();
 
     // draw hour ticks
+    ctx.lineWidth = HOUR_MARK_WIDTH;
     for(let i=0; i<12; i++){
         let angle = BIG_STEP*i;
         let c = Math.cos(angle);
         let s = Math.sin(angle);
         ctx.beginPath();
-        ctx.moveTo(HW + RADIUS*c, HH - RADIUS*s);
-        ctx.lineTo(HW + 0.9*RADIUS*c, HH - 0.9*RADIUS*s);
+        ctx.moveTo(RADIUS*c, RADIUS*s);
+        ctx.lineTo(0.9*RADIUS*c, 0.9*RADIUS*s);
         ctx.stroke();
     }
 
     // draw minute ticks
-    ctx.lineWidth = 1;
+    ctx.lineWidth = MINUTE_MARK_WIDTH;
     for(let i=1; i<60; i++){
         if((i%5) == 0){
             continue;
@@ -52,8 +67,8 @@ function drawHelper(ctx) {
         let c = Math.cos(angle);
         let s = Math.sin(angle);
         ctx.beginPath();
-        ctx.moveTo(HW + RADIUS*c, HH - RADIUS*s);
-        ctx.lineTo(HW + 0.95*RADIUS*c, HH - 0.95*RADIUS*s);
+        ctx.moveTo(RADIUS*c, RADIUS*s);
+        ctx.lineTo(0.95*RADIUS*c, 0.95*RADIUS*s);
         ctx.stroke();
     }
 
@@ -65,33 +80,33 @@ function drawHelper(ctx) {
     let hours = time.getHours() + minutes/60;
 
     // draw second hand
-    ctx.lineWidth = 1;
+    ctx.lineWidth = SECOND_HAND_WIDTH;
     ctx.beginPath();
-    ctx.moveTo(WIDTH/2, HEIGHT/2);
+    ctx.moveTo(0.0, 0.0);
     ctx.lineTo(
-        HW + SECOND_LENGTH * Math.cos(TAU/4 - SMALL_STEP*seconds),
-        HH - SECOND_LENGTH * Math.sin(TAU/4 - SMALL_STEP*seconds)
+        SECOND_LENGTH * Math.cos(QTAU - SMALL_STEP*seconds),
+        SECOND_LENGTH * Math.sin(QTAU - SMALL_STEP*seconds)
     );
     ctx.stroke();
 
-    ctx.strokeStyle = "black";
     // draw minute hand
-    ctx.lineWidth = 2;
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = MINUTE_HAND_WIDTH;
     ctx.beginPath();
-    ctx.moveTo(WIDTH/2, HEIGHT/2);
+    ctx.moveTo(0.0, 0.0);
     ctx.lineTo(
-        HW + MINUTE_LENGTH * Math.cos(TAU/4 - SMALL_STEP*minutes),
-        HH - MINUTE_LENGTH * Math.sin(TAU/4 - SMALL_STEP*minutes)
+        MINUTE_LENGTH * Math.cos(QTAU - SMALL_STEP*minutes),
+        MINUTE_LENGTH * Math.sin(QTAU - SMALL_STEP*minutes)
     );
     ctx.stroke();
 
     // draw hour hand
-    ctx.lineWidth = 5;
+    ctx.lineWidth = HOUR_HAND_WIDTH;
     ctx.beginPath();
-    ctx.moveTo(WIDTH/2, HEIGHT/2);
+    ctx.moveTo(0.0, 0.0);
     ctx.lineTo(
-        HW + HOUR_LENGTH * Math.cos(TAU/4 - BIG_STEP*hours),
-        HH - HOUR_LENGTH * Math.sin(TAU/4 - BIG_STEP*hours)
+        HOUR_LENGTH * Math.cos(QTAU - BIG_STEP*hours),
+        HOUR_LENGTH * Math.sin(QTAU - BIG_STEP*hours)
     );
     ctx.stroke();
 
