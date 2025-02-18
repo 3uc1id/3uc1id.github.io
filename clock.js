e2f1df3b00db35658f285cb48438c93d934dd634
Chris Calderon <calderonchristian73@gmail.com>
2025-02-18 14:02:10 -0800

    Optimized animation by reusing clock face, added vsync support.

--- clock.js
+++ clock.js
@@ -1,6 +1,6 @@
 const TAU = 6.283185307179586;
 const QTAU = TAU/4.0;
-const TICK = 100; // ms per frame
+// const TICK = 100; // ms per frame
 const RADIUS = 100; // radius of clock circle
 const WIDTH = 250; // canvas width
 const HEIGHT = 250; // canvas height
@@ -24,20 +24,23 @@ const HOUR_HAND_WIDTH = 5;
 
 
 function drawClock() {
-    canvas = document.getElementById("clock");
-    ctx = canvas.getContext("2d");
+    let canvas = document.getElementById("clock");
+    let ctx = canvas.getContext("2d");
     ctx.translate(HW, HH); // move origin to center of canvas
     ctx.scale(1.0, -1.0); // flip y axis so it goes up instead of down
-    drawHelper(ctx);
-}
+    
+    // set initial angles for the hands based on the current time.
+    let last_frame;
+    let start_time = new Date();
+    let seconds = start_time.getSeconds() + start_time.getMilliseconds()/1000;
+    let minutes = start_time.getMinutes() + seconds/60;
+    let hours = start_time.getHours() + minutes/60;
 
-function drawHelper(ctx) {
-    // clear the canvas
-    // Normally clearRect takes the top left corner of the rectangle to clear,
-    // along with the width and height. But since the coordinate transform flipped the
-    // y-axis, it needs the bottom left corner instead.
-    ctx.clearRect(-HW, -HH, WIDTH, HEIGHT);
+    let seconds_angle = QTAU - seconds*SMALL_STEP;
+    let minutes_angle = QTAU - minutes*SMALL_STEP;
+    let hours_angle = QTAU - hours*BIG_STEP;
 
+    // draw clock face once and save image data.
     // draw circular clock outline
     ctx.strokeStyle = "black";
     ctx.lineWidth = CLOCK_BORDER_WIDTH;
@@ -72,43 +75,68 @@ function drawHelper(ctx) {
         ctx.stroke();
     }
 
-    // draw the hands
-    ctx.strokeStyle = "red";
-    let time = new Date(Date.now());
-    let seconds = time.getSeconds() + time.getMilliseconds()/1000;
-    let minutes = time.getMinutes() + seconds/60;
-    let hours = time.getHours() + minutes/60;
+    // ctx.getImageData is not affected by ctx transforms
+    let clock_face = ctx.getImageData(0, 0, WIDTH, HEIGHT);
 
-    // draw second hand
-    ctx.lineWidth = SECOND_HAND_WIDTH;
-    ctx.beginPath();
-    ctx.moveTo(0.0, 0.0);
-    ctx.lineTo(
-        SECOND_LENGTH * Math.cos(QTAU - SMALL_STEP*seconds),
-        SECOND_LENGTH * Math.sin(QTAU - SMALL_STEP*seconds)
-    );
-    ctx.stroke();
+    function drawHelper(timestamp) {
+        if(last_frame === undefined){
+            last_frame=timestamp;
+            requestAnimationFrame(drawHelper);
+            return;
+        }
+        // clear the canvas
+        // Normally clearRect takes the top left corner of the rectangle to clear,
+        // along with the width and height. But since the coordinate transform flipped the
+        // y-axis, it needs the bottom left corner instead.
+        ctx.clearRect(-HW, -HH, WIDTH, HEIGHT);
+        // putImageData is not affected by the ctx transforms.
+        ctx.putImageData(clock_face, 0, 0);
+    
+        // draw the hands
+        ctx.strokeStyle = "red";
 
-    // draw minute hand
-    ctx.strokeStyle = "black";
-    ctx.lineWidth = MINUTE_HAND_WIDTH;
-    ctx.beginPath();
-    ctx.moveTo(0.0, 0.0);
-    ctx.lineTo(
-        MINUTE_LENGTH * Math.cos(QTAU - SMALL_STEP*minutes),
-        MINUTE_LENGTH * Math.sin(QTAU - SMALL_STEP*minutes)
-    );
-    ctx.stroke();
+        let timestep = (timestamp - last_frame)/1000;
+        seconds_angle -= (timestep%60)*SMALL_STEP;
+        timestep /= 60;
+        minutes_angle -= (timestep%60)*SMALL_STEP;
+        timestep /= 60;
+        hours_angle -= (timestep%12)*BIG_STEP;
+    
+        // draw second hand
+        ctx.lineWidth = SECOND_HAND_WIDTH;
+        ctx.beginPath();
+        ctx.moveTo(0.0, 0.0);
+        ctx.lineTo(
+            SECOND_LENGTH * Math.cos(seconds_angle),
+            SECOND_LENGTH * Math.sin(seconds_angle)
+        );
+        ctx.stroke();
+    
+        // draw minute hand
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = MINUTE_HAND_WIDTH;
+        ctx.beginPath();
+        ctx.moveTo(0.0, 0.0);
+        ctx.lineTo(
+            MINUTE_LENGTH * Math.cos(minutes_angle),
+            MINUTE_LENGTH * Math.sin(minutes_angle)
+        );
+        ctx.stroke();
+    
+        // draw hour hand
+        ctx.lineWidth = HOUR_HAND_WIDTH;
+        ctx.beginPath();
+        ctx.moveTo(0.0, 0.0);
+        ctx.lineTo(
+            HOUR_LENGTH * Math.cos(hours_angle),
+            HOUR_LENGTH * Math.sin(hours_angle)
+        );
+        ctx.stroke();
+        
+        last_frame=timestamp;
+        requestAnimationFrame(drawHelper);
+    }
+    
 
-    // draw hour hand
-    ctx.lineWidth = HOUR_HAND_WIDTH;
-    ctx.beginPath();
-    ctx.moveTo(0.0, 0.0);
-    ctx.lineTo(
-        HOUR_LENGTH * Math.cos(QTAU - BIG_STEP*hours),
-        HOUR_LENGTH * Math.sin(QTAU - BIG_STEP*hours)
-    );
-    ctx.stroke();
-
-    setTimeout(drawHelper, TICK, ctx);
+    requestAnimationFrame(drawHelper);
 }
